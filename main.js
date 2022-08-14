d0bcf18056390dc253d0f9b2f42733135dd42814
Archan Dhrangadharia <74826173+ArchanRD@users.noreply.github.com>
2022-08-14 12:45:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ let menu = document.querySelector(".menu");
 let menuItem = document.querySelector(".nav-item");
 
 function toggle(){
+    // console.log("menu")
     if(menuItem.classList.contains("active")){
         menuItem.classList.remove("active");
     }else{
         menuItem.classList.add("active");
     }
 }
+
+menuItem.addEventListener("click", () =>{
+    menuItem.classList.remove("active");
+});
